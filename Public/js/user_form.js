975084ff414f75b466cbe1957aad54a6ba285f14
山崎剛 <tsuyoshi_yamazaki@respawn.jp>
2021-10-27 00:30:47 +0900

    送信ボタンバリデーション

--- Public/js/user_form.js
+++ Public/js/user_form.js
@@ -3,10 +3,13 @@ let preview_1 = document.getElementById('preview_1');
 const fileInput_1 = document.getElementById('fileInput_1');
 let preview_2 = document.getElementById('preview_2');
 const fileInput_2 = document.getElementById('fileInput_2');
+const submit = document.getElementById('submit');
 
 //プレビューサイズの調整
 let previewWidth = 600;
 let previewHeight = 300;
+
+submit.disabled = true;
 
 fileInput_1.addEventListener('change', function(){
   handleFileSelect_1();
@@ -20,12 +23,14 @@ const handleFileSelect_1 = () => {
   preview_1.innerHTML = "";
   let file_1 = fileInput_1.files;
   previewFile(file_1[0], preview_1);
+  isFileCheck();
 }
 
 const handleFileSelect_2 = () => {
   preview_2.innerHTML = "";
   let file_2 = fileInput_2.files;
   previewFile(file_2[0], preview_2);
+  isFileCheck();
 }
 
 let uploadImage;
@@ -42,4 +47,10 @@ const previewFile = (file, previewSpace) => {
     //}
   }
   reader.readAsDataURL(file);
+}
+
+const isFileCheck = () => {
+  if(fileInput_1.files.length > 0 && fileInput_2.files.length > 0){
+    submit.disabled = false;
+  }
 }
